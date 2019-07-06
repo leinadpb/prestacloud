e331768d2755e4fe2db6752d3135e9ecd4f88249
Daniel Pena Berroa <digrape07@gmail.com>
2019-07-05 20:22:04 -0400

    return client in Client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -10,6 +10,7 @@ class Client < ApplicationRecord
         last_name: last_name,
         goverment_id: goverment_id,
         phone: phone,
+        email: email,
         loans: Loan.where(client_id: id).map(&:sanitazed_info_without_client),
         stripe_id: ::UserClient.find_by(goverment_id: goverment_id).get_stripe_id
     }
